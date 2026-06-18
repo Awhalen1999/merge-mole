f9433909c45d854915fe1114aa941aa8c520caf9
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2026-06-18 20:20:44 -0230

    Initial Commit

---
+++ MergeMole/MergeMoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MergeMoleApp.swift
+//  MergeMole
+//
+//  Created by Alex Whalen on 2026-06-18.
+//
+
+import SwiftUI
+
+@main
+struct MergeMoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
